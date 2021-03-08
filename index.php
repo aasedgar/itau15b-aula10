cf0f1fe2fd0d72a9984d23814f51114e4335a36d
EdgarAugusto <aas.edgar@gmail.com>
2021-03-08 14:43:29 -0300

    teste com terceiro layout

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
+
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Company Bootstrap Template Demo</title>
+  <title>BizPage Bootstrap Template Demo</title>
   <meta name="robots" content="noindex, nofollow">
     <link rel="stylesheet" href="https://bootstrapmade.com/theme/css/demo-2.3.0.css">
   </head>
@@ -17,27 +18,27 @@
     </div>
     <div class="preview-devices">
       <ul>
-        <li class="preview-test preview-devices-active" id="preview-test-desktop" title="Desktop preview of the  Company template"><a href=""><svg class="icon icon-preview"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#preview"/></svg></a></li>
-        <li class="preview-test" id="preview-test-tablet" title="Tablet preview of the Company template"><a href=""><svg class="icon icon-tablet"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#tablet"/></svg></a></li>
-        <li class="preview-test" id="preview-test-mobile" title="Mobile preview of the Company template"><a href=""><svg class="icon icon-smartphone"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#smartphone"/></svg></a></li>
+        <li class="preview-test preview-devices-active" id="preview-test-desktop" title="Desktop preview of the  BizPage template"><a href=""><svg class="icon icon-preview"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#preview"/></svg></a></li>
+        <li class="preview-test" id="preview-test-tablet" title="Tablet preview of the BizPage template"><a href=""><svg class="icon icon-tablet"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#tablet"/></svg></a></li>
+        <li class="preview-test" id="preview-test-mobile" title="Mobile preview of the BizPage template"><a href=""><svg class="icon icon-smartphone"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#smartphone"/></svg></a></li>
       </ul>
     </div>
 
     <div class="navigate">
       <ul>
-        <li><a href="https://bootstrapmade.com/demo/templates/Company/" target="_top" title="Hide the demo bar"><svg class="icon icon-external-link"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#external-link"/></svg></a></li>
-        <li><a href="https://bootstrapmade.com/company-free-html-bootstrap-template/" title="Company Template Home Page"><svg class="icon icon-home"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#home"/></svg></a></li>
-        <li><a class="download" href="https://bootstrapmade.com/company-free-html-bootstrap-template/#download" title="Download the Company Template"><svg class="icon icon-download"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#download"/></svg> <span>Free Download</span></a></li>
-                  <li><a href="https://bootstrapmade.com/demo/Dewi/" title="Previous Template: Dewi" target="_top"><svg class="icon icon-chevron-left"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-left"/></svg></a></li>
+        <li><a href="https://bootstrapmade.com/demo/templates/BizPage/" target="_top" title="Hide the demo bar"><svg class="icon icon-external-link"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#external-link"/></svg></a></li>
+        <li><a href="https://bootstrapmade.com/bizpage-bootstrap-business-template/" title="BizPage Template Home Page"><svg class="icon icon-home"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#home"/></svg></a></li>
+        <li><a class="download" href="https://bootstrapmade.com/bizpage-bootstrap-business-template/#download" title="Download the BizPage Template"><svg class="icon icon-download"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#download"/></svg> <span>Free Download</span></a></li>
+                  <li><a href="https://bootstrapmade.com/demo/NewBiz/" title="Previous Template: NewBiz" target="_top"><svg class="icon icon-chevron-left"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-left"/></svg></a></li>
         
-                <li><a href="https://bootstrapmade.com/demo/Techie/" title="Next Template: Techie" target="_top"><svg class="icon icon-chevron-right"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-right"/></svg></a></li>
+                <li><a href="https://bootstrapmade.com/demo/Reveal/" title="Next Template: Reveal" target="_top"><svg class="icon icon-chevron-right"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-right"/></svg></a></li>
               </ul>
     </div>
 
   </header>
 
   <div id="preview">
-    <iframe id="preview-frame" class="preview-desktop" src="https://bootstrapmade.com/demo/templates/Company/" frameborder="0"></iframe>
+    <iframe id="preview-frame" class="preview-desktop" src="https://bootstrapmade.com/demo/templates/BizPage/" frameborder="0"></iframe>
   </div>
 
       <script src="https://bootstrapmade.com/theme/js/demo-2.3.0.js"></script>
