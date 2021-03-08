f0552b05285f2e84d7a7a00cfed7f05fa80e6a0d
EdgarAugusto <aas.edgar@gmail.com>
2021-03-08 14:22:29 -0300

    inicio de troca de tema do bootstrap

--- index.php
+++ index.php
@@ -1,13 +1,62 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial</title>
-</head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Company Bootstrap Template Demo</title>
+  <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" href="https://bootstrapmade.com/theme/css/demo-2.3.0.css">
+  </head>
+
 <body>
-    
+  <header id="header">
+
+    <div class="logo">
+      <a href="https://bootstrapmade.com/" rel="home" title="Free Bootstrap Themes and Website Templates" title="Back to BootstrapMade Homepage"><img alt="BootstrapMade" src="https://bootstrapmade.com/theme/img/logo.png"><strong>Bootstrap<span>Made</span></strong></a>
+    </div>
+    <div class="preview-devices">
+      <ul>
+        <li class="preview-test preview-devices-active" id="preview-test-desktop" title="Desktop preview of the  Company template"><a href=""><svg class="icon icon-preview"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#preview"/></svg></a></li>
+        <li class="preview-test" id="preview-test-tablet" title="Tablet preview of the Company template"><a href=""><svg class="icon icon-tablet"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#tablet"/></svg></a></li>
+        <li class="preview-test" id="preview-test-mobile" title="Mobile preview of the Company template"><a href=""><svg class="icon icon-smartphone"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#smartphone"/></svg></a></li>
+      </ul>
+    </div>
+
+    <div class="navigate">
+      <ul>
+        <li><a href="https://bootstrapmade.com/demo/templates/Company/" target="_top" title="Hide the demo bar"><svg class="icon icon-external-link"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#external-link"/></svg></a></li>
+        <li><a href="https://bootstrapmade.com/company-free-html-bootstrap-template/" title="Company Template Home Page"><svg class="icon icon-home"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#home"/></svg></a></li>
+        <li><a class="download" href="https://bootstrapmade.com/company-free-html-bootstrap-template/#download" title="Download the Company Template"><svg class="icon icon-download"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#download"/></svg> <span>Free Download</span></a></li>
+                  <li><a href="https://bootstrapmade.com/demo/Dewi/" title="Previous Template: Dewi" target="_top"><svg class="icon icon-chevron-left"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-left"/></svg></a></li>
+        
+                <li><a href="https://bootstrapmade.com/demo/Techie/" title="Next Template: Techie" target="_top"><svg class="icon icon-chevron-right"  fill="currentColor"><use xlink:href="https://bootstrapmade.com/theme/img/icons-2.3.0.svg#chevron-right"/></svg></a></li>
+              </ul>
+    </div>
+
+  </header>
+
+  <div id="preview">
+    <iframe id="preview-frame" class="preview-desktop" src="https://bootstrapmade.com/demo/templates/Company/" frameborder="0"></iframe>
+  </div>
+
+      <script src="https://bootstrapmade.com/theme/js/demo-2.3.0.js"></script>
+    <script>
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-55234356-4', 'auto');
+    ga('send', 'pageview');
+  </script>
 </body>
+
 </html>
-<div>Bem-vindo à Página do Edgar</div>
